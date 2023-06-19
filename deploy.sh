ba57b1d9c425648453fbe517d90875324176a3f2
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-06-19 12:45:02 +0200

    Add Code

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Ga naar de directory waar je Flask-toepassing is geïnstalleerd
+cd /home/farm
+
+# Haal de laatste wijzigingen op van de repository
+git pull
+
+# Installeer of update eventuele afhankelijkheden
+pip install -r requirements.txt
+
+# Herstart de toepassing met Gunicorn en Supervisor
+supervisorctl stop main.py
+supervisorctl start main.py
+
+# Optioneel: Controleer of de toepassing correct wordt uitgevoerd
+systemctl status main.py
