db23ba062676d026c6fa7dd3165839e192e90e55
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-06-20 14:15:21 +0200

    Add Code

---
+++ main.py
@@ -0,0 +1,24 @@
+# Add your code after this line
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<p>Home, sweet home.</p>"
+
+
+@app.route("/greet/")
+def greet():
+    return "<h1>Hello, world!</h1>"
+
+
+@app.route("/greet/<name>")
+def greet_name(name):
+    return f"<h1>Hello, {name}!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
