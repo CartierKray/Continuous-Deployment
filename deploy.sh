24b8b19678b423899f6d3f6e8cc0fb8f337e85bd
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-06-23 12:37:04 +0200

    Finish

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# Ga naar de directory waar je Flask-toepassing is geïnstalleerd
-cd /home/farm
-
-# Haal de laatste wijzigingen op van de repository
-git pull
-
-# Herstart de toepassing met Gunicorn en Supervisor
-supervisorctl stop main.py
-supervisorctl start main.py
-
-# Controleer of de toepassing correct wordt uitgevoerd
-systemctl status main.py
 
